2a73490fae6c8a51279ba813bf0eb8a525e67df9
Linio <hasanabdurahman336@gmail.com>
2025-09-06 23:21:33 +0700

    Implicit Intent

--- app/src/main/java/com/alopeworld/myintentappexplixit/MainActivity.kt
+++ app/src/main/java/com/alopeworld/myintentappexplixit/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.alopeworld.myintentappexplixit
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -23,6 +24,10 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         val btnMoveWithObject:Button = findViewById(R.id.btn_move_activity_object)
         btnMoveWithObject.setOnClickListener(this)
+
+        val btnDialPhone:Button = findViewById(R.id.btn_dial_number)
+        btnDialPhone.setOnClickListener(this)
+
     }
 
     override fun onClick(v: View?) {
@@ -50,6 +55,12 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 val moveWithObjectIntent = Intent(this@MainActivity, MoveWithObjectActivity::class.java)
                 moveWithObjectIntent.putExtra(MoveWithObjectActivity.EXTRA_PERSON, person)
                 startActivity(moveWithObjectIntent)
+            }
+
+            R.id.btn_dial_number -> {
+                val phoneNumber = "081212121212"
+                val dialPhoneIntent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:$phoneNumber"))
+                startActivity(dialPhoneIntent)
             }
         }
     }
